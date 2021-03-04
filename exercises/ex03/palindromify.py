326063a703d403e7ea74972e6311a8ea7ab26515
rohan jain <rohanj@email.unc.edu>
2021-03-03 22:48:39 -0500

    Final

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,12 +1,32 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730407394"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
+    print(palindromify("race", False))
+    print(palindromify("han", True))
+    print(palindromify("red", True))
+    print(palindromify("live on time ", False))
+
+
+def palindromify(x: str, y: bool) -> str:
+    """Palindromify's a number."""
+    if y is True:
+        i = len(x) - 1
+        a = list(x)
+        while i > -1:
+            a.append(a[i])
+            i -= 1
+    if y is False:
+        i = len(x) - 2
+        a = list(x)
+        while i > -1:
+            a.append(a[i])
+            i -= 1
+    str1: str = "".join(a)
+    return str1
 
 
 if __name__ == "__main__":
--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -1,12 +1,44 @@
 """EX03 - prime functions."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730407394"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(is_prime(5)), print(list_primes(10, 20))
+    print(is_prime(0))
+    print(is_prime(6))
+    print(is_prime(31))
+    print(is_prime(110)) 
+    print(list_primes(-1, 5))   
+
+
+def is_prime(x: int) -> bool:
+    """Is the number prime?"""
+    divider: int = 2
+    if x < 2:
+        return False
+    while divider < x + 1:
+        remainder = x % divider
+        if divider == x:
+            return True
+        else:
+            if remainder != 0:
+                divider += 1
+            if remainder == 0:
+                return False
+
+
+def list_primes(x: int, y: int) -> list[int]:
+    """Makes a list of primes."""
+    a = []
+    while x < y:
+        prime_check = is_prime(x)
+        if prime_check is True:
+            a.append(x)
+            x += 1
+        if prime_check is False:
+            x += 1
+    return a
 
 
 if __name__ == "__main__":
