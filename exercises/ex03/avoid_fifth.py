adeaa03190fc2c39bfe427e772afc977de9538d6
rohan jain <rohanj@email.unc.edu>
2021-03-03 22:48:22 -0500

    Final

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,12 +1,31 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730407394"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(avoid_fifth("hello there"))
+    print(avoid_fifth("hello"))
+    print(avoid_fifth("Easer"))
+
+
+def avoid_fifth(x: str) -> str:
+    """Takes out the letter e."""
+    i: int = 0
+    a = list(x)
+    length = len(x)
+    while i < length:
+        if a[i] == "e":
+            a.pop(i)
+            length -= 1
+        else:
+            if a[i] == "E":
+                a.pop(i)
+                length -= 1
+            else:
+                i += 1
+    str1 = "".join(a)
+    return str1
 
 
 if __name__ == "__main__":
